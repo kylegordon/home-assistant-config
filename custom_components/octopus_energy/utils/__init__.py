7e7dd698ecaa8c32fee8f63ba48683b15a095671
Kyle Gordon <kyle@glasgownet.com>
2024-04-21 13:36:27 +0100

    Octopus Energy 10.3.1

--- custom_components/octopus_energy/utils/__init__.py
+++ custom_components/octopus_energy/utils/__init__.py
@@ -3,7 +3,7 @@ import re
 from datetime import datetime, timedelta
 
 
-from homeassistant.util.dt import (as_utc, parse_datetime)
+from homeassistant.util.dt import (as_local, as_utc, parse_datetime)
 
 from ..const import (
   REGEX_TARIFF_PARTS,
@@ -64,7 +64,8 @@ def get_active_tariff_code(utcnow: datetime, agreements):
   return None
 
 def get_off_peak_cost(current: datetime, rates: list):
-  today_start = as_utc(current.replace(hour=0, minute=0, second=0, microsecond=0))
+  # Need to use as local to ensure we get the correct from/to periods relative to our local time
+  today_start = as_utc(as_local(current).replace(hour=0, minute=0, second=0, microsecond=0))
   today_end = today_start + timedelta(days=1)
   off_peak_cost = None
 
@@ -88,6 +89,35 @@ def is_off_peak(current: datetime, rates):
           rate_information is not None and 
           rate_information["current_rate"]["is_intelligent_adjusted"] == False and 
           value_inc_vat_to_pounds(off_peak_value) == rate_information["current_rate"]["value_inc_vat"])
+
+class OffPeakTime:
+  start: datetime
+  end: datetime
+
+  def __init__(self, start, end):
+    self.start = start
+    self.end = end
+
+def get_off_peak_times(current: datetime, rates: list, include_intelligent_adjusted = False):
+  off_peak_value = get_off_peak_cost(current, rates)
+  times: list[OffPeakTime] = []
+
+  if rates is not None and off_peak_value is not None:
+    start = None
+    rates_length = len(rates)
+    for rate_index in range(rates_length):
+      rate = rates[rate_index]
+      if (rate["value_inc_vat"] == off_peak_value and 
+          ("is_intelligent_adjusted" not in rate or rate["is_intelligent_adjusted"] == False or include_intelligent_adjusted)):
+        if start is None:
+          start = rate["start"]
+      elif start is not None:
+        end = rates[rate_index - 1]["end"]
+        if end >= current:
+          times.append(OffPeakTime(start, end))
+        start = None
+
+  return times
 
 def private_rates_to_public_rates(rates: list):
   if rates is None:
