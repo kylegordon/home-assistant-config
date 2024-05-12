90b4b52b7bbf5e597306da1040634b8458b41333
Kyle Gordon <kyle@glasgownet.com>
2024-05-12 21:24:07 +0100

    Octopus Energy v10.3.1

--- custom_components/octopus_energy/electricity/current_accumulative_consumption_peak.py
+++ custom_components/octopus_energy/electricity/current_accumulative_consumption_peak.py
@@ -32,7 +32,7 @@ _LOGGER = logging.getLogger(__name__)
 class OctopusEnergyCurrentAccumulativeElectricityConsumptionPeak(MultiCoordinatorEntity, OctopusEnergyElectricitySensor, RestoreSensor):
   """Sensor for displaying the current days accumulative electricity reading during peak hours."""
 
-  def __init__(self, hass: HomeAssistant, coordinator, rates_coordinator, standing_charge_coordinator, meter, point):
+  def __init__(self, hass: HomeAssistant, coordinator, rates_coordinator, standing_charge_coordinator, tariff_code, meter, point):
     """Init sensor."""
     MultiCoordinatorEntity.__init__(self, coordinator, [rates_coordinator, standing_charge_coordinator])
     OctopusEnergyElectricitySensor.__init__(self, hass, meter, point)
@@ -40,6 +40,7 @@ class OctopusEnergyCurrentAccumulativeElectricityConsumptionPeak(MultiCoordinato
     self._state = None
     self._last_reset = None
     
+    self._tariff_code = tariff_code
     self._rates_coordinator = rates_coordinator
     self._standing_charge_coordinator = standing_charge_coordinator
 
@@ -109,7 +110,8 @@ class OctopusEnergyCurrentAccumulativeElectricityConsumptionPeak(MultiCoordinato
       consumption_data,
       rate_data,
       standing_charge,
-      None # We want to always recalculate
+      None, # We want to always recalculate
+      self._tariff_code
     )
 
     if (consumption_and_cost is not None):
@@ -131,9 +133,6 @@ class OctopusEnergyCurrentAccumulativeElectricityConsumptionPeak(MultiCoordinato
     
     if state is not None and self._state is None:
       self._state = None if state.state in (STATE_UNAVAILABLE, STATE_UNKNOWN) else state.state
-      
-      # For some reason this sensor is having issues with HA recognising last_reset updating unless we update it like the following :shrug:
-      self._attributes = dict_to_typed_dict(state.attributes, ["last_reset"])
-      self._last_reset = datetime.fromisoformat(state.attributes["last_reset"]) if "last_reset" in state.attributes else None
+      self._attributes = dict_to_typed_dict(state.attributes)
 
       _LOGGER.debug(f'Restored OctopusEnergyCurrentAccumulativeElectricityConsumptionPeak state: {self._state}')
--- custom_components/octopus_energy/intelligent/charge_limit.py
+++ custom_components/octopus_energy/intelligent/charge_limit.py
@@ -37,10 +37,6 @@ class OctopusEnergyIntelligentChargeLimit(CoordinatorEntity, RestoreNumber, Octo
     self._attributes = {}
     self.entity_id = generate_entity_id("number.{}", self.unique_id, hass=hass)
 
-    self._attr_native_min_value = 10
-    self._attr_native_max_value = 100
-    self._attr_native_step = 5
-
   @property
   def unique_id(self):
     """The id of the sensor."""
@@ -109,7 +105,7 @@ class OctopusEnergyIntelligentChargeLimit(CoordinatorEntity, RestoreNumber, Octo
         (last_number_data := await self.async_get_last_number_data())
       ):
       
-      self._attributes = dict_to_typed_dict(last_state.attributes, ["min", "max", "step"])
+      self._attributes = dict_to_typed_dict(last_state.attributes)
       if last_state.state not in (STATE_UNAVAILABLE, STATE_UNKNOWN):
         self._state = last_number_data.native_value
           
